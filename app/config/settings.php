c08fc2ee2584c05fe82fee82db70e26043e4c662
Tyrone <tyrone@clearmindai.com>
2025-01-16 20:09:42 +0800

    update yros

--- app/config/settings.php
+++ app/config/settings.php
@@ -3,7 +3,7 @@
  * Yros app settings
  */
 
-$app_settings['page_guide'] = false; // display route and path details below.
+$app_settings['page_guide'] = true; // display route and path details below.
 
 $app_settings['error_log'] = false; //Log all php errors // find logs @ app/system/logs/error_logs
 
@@ -13,7 +13,7 @@ $app_settings['views_log'] = true; //Log all view calls
 
 $app_settings['encryption_key'] = "yros"; //set up unique encryption key for security
 
-$app_settings['port'] = "5015"; // app server port: example 5010 in http://localhost:5010
+$app_settings['port'] = "5010"; // app server port: example 5010 in http://localhost:5010
 
 $app_settings['enable_curly_template'] = false; //Enable {{}} in views files
 
@@ -23,6 +23,7 @@ $app_settings['single_route'] = false; // if page route has set, you can't navig
 
 //$app_settings['project_root_url'] is the root url of the app. Ex: http://localhost:5010
 $app_settings['project_root_url'] = "";   // Leave it empty/null for auto detect root url
+$app_settings['local_ip'] = "192.168.254.110";
 
 
 
